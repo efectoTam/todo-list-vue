8d893bc2cdc46fd545e6c8223223c045b98e49f6
Tamara Luque <tam.luque@gmail.com>
2019-08-14 17:30:17 -0400

    todo list ready

--- js/vue.js
+++ js/vue.js
@@ -1,6 +1,36 @@
-new Vue ({
+var app = new Vue ({
   el: '#app',
   data: {
-    titulo: 'Lista de Tareas'
+    titulo: 'Lista de Tareas',
+    tasks: [
+      {
+        texto: 'Aprender Vue.js',
+        finished: false
+      },
+      {
+        texto: 'Aprender Angular',
+        finished: false
+      },
+      {
+        texto: 'Aprender React',
+        finished: false
+      }
+    ],
+    newTask: ''
+  },
+  methods: {
+    addTask: function () {
+      var texto =  this.newTask.trim();
+      if(texto) {
+        this.tasks.push({
+          texto: texto,
+          finished: false
+        })
+      }
+      this.newTask = '';
+    },
+    removeTask: function (index) {
+      this.tasks.splice(index,1)
+    }
   }
 })
